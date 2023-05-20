c43ffe56190db1ed7cdfe3792bbbdf30567dad2a
Anna Kravets <annakravets1906@gmail.com>
2023-05-20 21:08:19 +0300

    Added change buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,33 +6,28 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-// const useButtonClick = (key, button) => {
-//   const [state, setState] = useState(prevState => {
-//     return {
-//       [button.toLowerCase()]: prevState[button.toLowerCase()] + 1,
-//     };
-//   });
-// };
-
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleLeaveFeedback = () => {
-    // console.log(button);
+  const handleLeaveFeedback = button => {
+    switch (button) {
+      case 'Good':
+        setGood(prevGood => prevGood + 1);
+        break;
 
-    setGood(prevGood => prevGood + 1);
+      case 'Neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
 
-    setNeutral(prevNeutral => prevNeutral + 1);
+      case 'Bad':
+        setBad(prevBad => prevBad + 1);
+        break;
 
-    // console.log(prevGood);
-    // setGood(useButtonClick(good));
-    // this.setState(prevState => {
-    //   return {
-    //     [button.toLowerCase()]: prevState[button.toLowerCase()] + 1,
-    //   };
-    // });
+      default:
+        break;
+    }
   };
 
   const countTotalFeedback = good + neutral + bad;
@@ -66,54 +61,3 @@ export default function App() {
     </Wrapper>
   );
 }
-
-// export default class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleLeaveFeedback = button => {
-//     this.setState(prevState => {
-//       return {
-//         [button.toLowerCase()]: prevState[button.toLowerCase()] + 1,
-//       };
-//     });
-//   };
-
-//   render() {
-//     const countTotalFeedback =
-//       this.state.good + this.state.neutral + this.state.bad;
-
-//     const countPositiveFeedbackPercentage = Math.round(
-//       (this.state.good / countTotalFeedback) * 100
-//     );
-
-//     const visible =
-//       this.state.good > 0 || this.state.neutral > 0 || this.state.bad > 0;
-
-//     return (
-//       <Wrapper>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={['Good', 'Neutral', 'Bad']}
-//             onLeaveFeedback={this.handleLeaveFeedback}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {!visible && <Notification message="There is no feedback" />}
-//           {visible && (
-//             <Statistics
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={countTotalFeedback}
-//               positivePercentage={countPositiveFeedbackPercentage}
-//             />
-//           )}
-//         </Section>
-//       </Wrapper>
-//     );
-//   }
-// }
